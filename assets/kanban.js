7df5f8016c9fe3cc336514a5f09316e8f6dad4ab
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-10-29 17:21:11 +0100

    Added the Modal and its Trigger with Js

---
+++ assets/kanban.js
@@ -0,0 +1,9 @@
+// Function to show the modal
+function openModal() {
+    document.getElementById("myModal").classList.remove("hidden");
+}
+
+// Function to hide the modal
+function closeModal() {
+    document.getElementById("myModal").classList.add("hidden");
+}
